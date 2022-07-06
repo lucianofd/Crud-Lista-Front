8d7b2486364f7d3063aee8bcc8930a77c4206b71
lucianoFD <dlffuhr@gmail.com>
2022-07-06 19:06:47 -0300

    varios

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginUsuario } from '../models/login-usuario';
+import { AuthoService } from '../service/autho.service';
+import { TokenService } from '../service/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isLogged = false;
+  isLoginFail = false;
+  loginUsuario: LoginUsuario;
+  nombreUsuario: string;
+  password: string;
+  roles: string[] =[];
+  errmsj: string;
+
+  constructor(
+    private  tokenService: TokenService,
+    private authService: AuthoService,
+    private router: Router
+
+  ) { }
 
   ngOnInit(): void {
+    //comprobar si esta logueado
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+
+  }
+
+  onLogin(): void {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      data => {
+        this.isLogged = true;
+        this.isLoginFail = false;
+
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        this.router.navigate(['/']);
+      },
+      err =>{
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errmsj = err.error.mensajes;
+        console.log(err.err.message);
+
+      }
+    )
+    
   }
 
 }
--- src/app/auth/registro.component.ts
+++ src/app/auth/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../service/token.service';
+import { AuthoService } from '../service/autho.service';
+import { Router } from '@angular/router';
+import { NuevoUsuario } from '../models/nuevo-usuario';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nuevoUsuario: NuevoUsuario;
+  nombre: string;
+  nombreUsuario: string;
+  email: string;
+  password: string;
+  errMsj: string;
+  isLogged = false;
 
-  ngOnInit(): void {
+  constructor(
+    private tokenService: TokenService,
+    private authoService: AuthoService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    }
   }
+/*
+  onRegister(): void {
+    this.nuevoUsuario = new NuevoUsuario(this.nombre, this.nombreUsuario, this.email, this.password);
+    this.authService.nuevo(this.nuevoUsuario).subscribe(
+      data => {
+        this.toastr.success('Cuenta Creada', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
 
+        this.router.navigate(['/login']);
+      },
+      err => {
+        this.errMsj = err.error.mensaje;
+        this.toastr.error(this.errMsj, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        // console.log(err.error.message);
+      }
+    );
+  }
+*/
 }
